07e5edd17fb1ed335ab65f60ce2367ba3d186cc2
Vitor <castanheira@discente.ufg.br>
2023-08-03 17:54:56 -0300

    add rating function

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 document.getElementById("valor-calculado").style.display = "none";
 document.getElementById("each").style.display = "none";
 
-function calculateDinner(){
+function calculateDinner() {
     var totalConta = document.getElementById("total-conta").value;
     var taxaServico = document.getElementById("taxa-servico").value;
     var numeroPessoas = document.getElementById("numero-pessoas").value;
 
-    if(totalConta === "" || taxaServico == 0 || isNaN(totalConta) || isNaN(numeroPessoas)){
+    if (totalConta === "" || taxaServico == 0 || isNaN(totalConta) || isNaN(numeroPessoas)) {
         alert("Por favor, coloque um valor válido!");
         return;
     }
 
-    if (numeroPessoas === "" || numeroPessoas <= 1){
+    if (numeroPessoas === "" || numeroPessoas <= 1) {
         numeroPessoas = 1;
         document.getElementById("each").style.display = "none"
     } else {
@@ -33,13 +33,13 @@ function limparCampos() {
     document.getElementById("numero-pessoas").value = "";
     document.getElementById("valor-calculado").style.display = "none";
     document.getElementById("each").style.display = "none";
-  }
+}
 
-document.getElementById("calcular").onclick = function(){
+document.getElementById("calcular").onclick = function () {
     calculateDinner();
 }
 
-document.getElementById("limpar").onclick = function(){
+document.getElementById("limpar").onclick = function () {
     limparCampos();
 }
 
@@ -47,4 +47,29 @@ const chk = document.getElementById('chk');
 
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark');
+});
+
+const openPopupButton = document.getElementById('openPopup');
+const closePopupButton = document.getElementById('closePopup');
+const popupContainer = document.getElementById('popupContainer');
+
+openPopupButton.addEventListener('click', () => {
+    popupContainer.style.display = 'flex';
+});
+
+closePopupButton.addEventListener('click', () => {
+    popupContainer.style.display = 'none';
+});
+
+var stars = document.querySelectorAll('.star-icon');
+                  
+document.addEventListener('click', function(e){
+  var classStar = e.target.classList;
+  if(!classStar.contains('ativo')){
+    stars.forEach(function(star){
+      star.classList.remove('ativo');
+    });
+    classStar.add('ativo');
+    console.log(e.target.getAttribute('data-avaliacao'));
+  }
 });
